e9f722bee9b616d8c77fc0f3c1261969f39d452f
ClintBrown1 <42039915+ClintBrown1@users.noreply.github.com>
2018-10-24 08:24:08 -0600

    Fixed typo "stiil" to "still" line 12

--- Xamarin.Essentials/Connectivity/Connectivity.shared.cs
+++ Xamarin.Essentials/Connectivity/Connectivity.shared.cs
@@ -9,7 +9,7 @@ namespace Xamarin.Essentials
         static event EventHandler<ConnectivityChangedEventArgs> ConnectivityChangedInternal;
 
         // a cache so that events aren't fired unnecessarily
-        // this is mainly an issue on Android, but we can stiil do this everywhere
+        // this is mainly an issue on Android, but we can still do this everywhere
         static NetworkAccess currentAccess;
         static List<ConnectionProfile> currentProfiles;
 
